8196c9fdb21914f895908aeef9e5a220945d1d53
Io-tiny <414344787@qq.com>
2020-11-20 14:34:13 +0800

    userEffect

--- learn-react/src/Example.js
+++ learn-react/src/Example.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Example() {
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = `点击了${count}次`
+  })
   return (
     <div>
       <p>点击了{count}</p>
