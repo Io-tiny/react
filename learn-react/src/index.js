5d7153c13de011d9ea0eba38ead55f16e2284234
Io-tiny <414344787@qq.com>
2021-01-14 11:43:39 +0800

    useContext使用

--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -1,9 +1,37 @@
-import React from 'react';
+// eslint-disable-next-line
+import React, { useContext, useLocalStore } from 'react';
 import ReactDOM from 'react-dom';
-import Example from './Example'
 
-ReactDOM.render(
-  <Example />,
-  document.getElementById('root')
-);
+export const StoreContext = React.createContext(null);
+
+export const useStore = () => {
+  const store = React.useContext(StoreContext);
+  return store;
+}
+export const store = {
+  a: {
+    todo: '1'
+  },
+  b: {
+    todo: '2'
+  }
+}
+
+
+const App = () => {
+  return (
+    <StoreContext.Provider value={store}>
+      <AA></AA>
+    </StoreContext.Provider>
+  );
+}
+const AA = () => {
+  return (
+    <p>haha</p>
+  )
+}
+
+ReactDOM.render((
+  <App></App>
+), document.getElementById('root'))
 
