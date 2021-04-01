e5ab68e79126fb16eeac145081b4aecb2883617e
Io-tiny <414344787@qq.com>
2021-04-01 23:32:31 +0800

    图片切换demo

--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -2,13 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const h1 = (<h1>黑哥真帅<span> 淦 </span><span>骗人</span></h1>)
+const url = ['./img/3.jpg', './img/4.jpg', './img/5.jpg']
+let i = 0;
+let timer = setInterval(() => {
+  if (i === 3) {
+    i = 0;
+  }
+  ReactDOM.render(
+    <img src={url[i]} alt="." />,
+    document.getElementById('container')
+  );
+  i++;
+}, 1000);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+document.getElementById('container').onmouseenter = () => {
+  clearInterval(timer);
+}
+document.getElementById('container').onmouseleave = () => {
+  clearInterval(timer);
+  timer = setInterval(() => {
+    if (i === 3) {
+      i = 0;
+    }
+    ReactDOM.render(
+      <img src={url[i]} alt="." />,
+      document.getElementById('container')
+    );
+    i++;
+  }, 1000);
+}
